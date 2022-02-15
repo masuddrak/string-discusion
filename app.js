50f8fc2e963952cf9f062e9db2d91747db90bf94
masudrana <masudranaacb@gmail.com>
2022-02-15 11:48:48 +0600

    startsWith searching

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const items=[
     '22 green laptop',
     's85 red phone',
     'laptop 888jg gray',
-    'phone jk88 green'
+    'phone jk88 green',
+    'phone jsk88 green',
 ]
 const phone=[];
 const search='Phone';
 for(const item of items){
-    if(item.toLowerCase().includes(search.toLowerCase())){
+    if(item.toLowerCase().startsWith(search.toLowerCase())){
         phone.push(item);
     }
 }
